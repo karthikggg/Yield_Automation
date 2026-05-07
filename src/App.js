380fabeb308fdfd75005fcd23c6101386e75c07b
Your Name <your.email@example.com>
2026-05-07 15:09:27 +0530

    responce

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { HashRouter } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <HashRouter basename="/Yield_Automation">
+      <HashRouter>
       <Header />
         <Routes>
           {/* <Route path="/episodes-change" element={<EPISODESCHANGE />} />
